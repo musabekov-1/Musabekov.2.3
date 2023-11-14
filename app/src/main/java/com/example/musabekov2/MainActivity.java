1e17321a02ef29330672cadd3051b4fa11a2c752
musabekov-1 <bestmusabekov@gmail.com>
2023-11-14 16:56:31 +0600

    Initial commit

---
+++ app/src/main/java/com/example/musabekov2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.musabekov2;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragmentForContainer, new FragmentA()).commit();
+    }
+}
